d6f9a7eeec1c522744080fdfea20209adb0972a6
bobinlee <bobeen.lee@huiseoul.com>
2020-06-21 15:17:40 +0900

    fix: update webpack

--- webpack.common.js
+++ webpack.common.js
@@ -3,39 +3,36 @@ const path = require("path");
 module.exports = {
   entry: {
     popup: path.join(__dirname, "src/popup/index.tsx"),
-    eventPage: path.join(__dirname, "src/eventPage.ts")
+    eventPage: path.join(__dirname, "src/eventPage.ts"),
   },
   output: {
     path: path.join(__dirname, "dist/js"),
-    filename: "[name].js"
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: require.resolve("babel-loader"),
-        options: {
-          presets: [["react-app", { flow: false, typescript: true }]]
-        }
+        loader: "babel-loader",
       },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader" // Creates style nodes from JS strings
+            loader: "style-loader", // Creates style nodes from JS strings
           },
           {
-            loader: "css-loader" // Translates CSS into CommonJS
+            loader: "css-loader", // Translates CSS into CommonJS
           },
           {
-            loader: "sass-loader" // Compiles Sass to CSS
-          }
-        ]
-      }
-    ]
+            loader: "sass-loader", // Compiles Sass to CSS
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  }
+    extensions: [".ts", ".tsx", ".js"],
+  },
 };
